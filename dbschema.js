badbbb62e8f505a8b2e51714cb8dd9c47db180e9
janimaenpaa <itsmayd@gmail.com>
2020-02-23 03:14:10 +0200

    Add login, signup, screams routes

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  scream: [
+    {
+      body: "user",
+      userHandle: "scream body",
+      createdAt: "2020-02-22T19:11:25.817Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
